c30bf75b7fbe9fc5e4b654c106fb46584cdb0d61
Abhay1kumar <kumarabhay401@gmail.com>
2022-02-22 13:12:37 +0530

    first-portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <header className="nav center">
+      <p className="logo">LOGO</p>
+      <input type="checkbox" name="" className="checkBtn"/>
+      <ol className="center">
+        <a href="#"><li>About</li></a>
+        <a href="#"><li>Education</li></a>
+        <a href="#"><li>Experience</li></a>
+        <a href="#"><li>Projects</li></a>
+        <a href="#"><li>Contact</li></a>
+      </ol>
+    </header>
+   
 
-export default App;
+    <section className="main">
+      <div className="box">
+        <h1>Abhay Kumar</h1>
+        <p>Front-End Developer & UI/UX</p>
+        <button className="btn">About</button>
+      </div>
+      <img src="https://lh3.googleusercontent.com/JYiKl0JH_hF4pGNgxJb0HEywbC1lbFRaMMEN9JJ58D1SJavkAqWNn8yq4qzsbS-99M7v3ZZwv5cqsYDnQjC3iPdNtVWdNJvCj7wswcFQhFA4UU3MnDxXb6kgAM3EWSfLPvWCeEvWI4_057I3VEMQKjfapvdq956d6wWScutMSsly6yLyjk-_cIs7XZRiObzB3ZZ9G0jXP4kiepWy0hbEXarJ8JADEdOI1A3ZBy4ENL0ir2TJuNZkchpG93mkBRI_p9bj2CRMQDQufBq8aAvLRNXBsG9bZbX5LCZDaR41DeZX1zHiyLAS9VJFatHLSpiB7Vm0OXvpYV70zUmk5dTUHc704KauctjOF4QMZrvjZEoeIClwshSpmrGhIcV_CVNrRu2vGWPWvdUagkc9hw6YAJWhfhUgfQXXV6dLA71FjC33V9KySHxX5ywllErXP5sQYmioGjI75XGvGzlENGw6JIUrlLpabADRQZ6gQCLNdiUkW_ios8XX5LMrAZt_Zqs1oKxMf_4uKEyGi7_xmYSV_eP7RO2RccSYbdSYUsF48vxAryqGdmxDRDOJBnTE998XQ3Fm3yzEoN7j2KVy7oB1F9aHxDpnWdHZHtVyJo_g6Su2HyCafASPwZSvw1NsqbI7VuCYZ-6rx4xYihZ8GOwCeWGdtd6NMJNI5lE2Lgt-nKSjB0bIO26KxyFbyd2VWdGgpjMgHsvermqNTO15P_TegVi8-w=s914-no?authuser=0" alt="image" />
+    </section>
+    
+    
+    </>
+  )
+}
